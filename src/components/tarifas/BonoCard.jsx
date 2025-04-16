7b00d3fc23651a3ab730f9d9653cd24afb5395e2
Aleiiixx <aleixrosellmorell@gmail.com>
2025-04-16 22:29:40 +0200

    tarifas

---
+++ src/components/tarifas/BonoCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const BonoCard = ({ tituloBono, precio, caracteristicas }) => {
+  return (
+    <div className="flex flex-col items-center w-fit min-w-[300px] max-h-[600px] max-w-[450px] bg-white p-1 shadow-lg rounded-2xl overflow-hidden">
+      <div className="w-full h-full rounded-2xl py-4 px-6 gap-4">
+        <div className="flex flex-col w-full py-4">
+          <h3
+            className="text-3xl font-bold text-black text-center"
+            dangerouslySetInnerHTML={{ __html: tituloBono }}
+          />
+          <div className="flex flex-row justify-center items-end">
+            <h4 className="text-3xl font-bold text-theme-violet-text text-center">{precio}</h4>
+          </div>
+        </div>
+
+        {caracteristicas.map((caracteristica, index) => (
+          <div key={index} className="flex flex-row pl-4">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-primary-blue" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M21 7L9 19l-5.5-5.5l1.41-1.41L9 16.17L19.59 5.59z" />
+            </svg>
+            <div className="font-sans pl-1 text-black">{caracteristica}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BonoCard;
---
+++ src/components/tarifas/tarifas.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import BonoCard from './BonoCard';
+import CarouselTarifas from './Carousel/CarouselTarifas';
+import ExtraCard from './ExtraCard';
+import PlanCard from './PlanCard';
+
+const ClasesSection = () => {
+  return (
+    <section id="clases" className="relative flex flex-col w-full overflow-hidden py-12 mb-24 gap-6">
+      <h2 className="text-theme-white">ELIGE TU BONO</h2>
+
+      <div className="hidden md:flex flex-row justify-center gap-8">
+        <BonoCard
+          tituloBono="BONO 1 SESION"
+          precio="10€"
+          caracteristicas={[]}
+        />
+        <BonoCard
+          tituloBono="BONO 5 SESIONES"
+          precio="40€"
+          caracteristicas={[]}
+        />
+        <BonoCard
+          tituloBono="BONO 10 SESIONES"
+          precio="75€"
+          caracteristicas={[]}
+        />
+      </div>
+      <div className='md:hidden'>
+        <CarouselTarifas
+          components={[
+            <BonoCard tituloBono="BONO 1 <br> SESION" precio="10€" caracteristicas={[]}/>,
+            <BonoCard tituloBono="BONO 5 <br> SESIONES" precio="40€" caracteristicas={[]}/>,
+            <BonoCard tituloBono="BONO 10 <br>SESIONES" precio="75€" caracteristicas={[]}/>
+          ]}
+        />
+      </div>
+
+      <h2 className="text-theme-white">PLANES MENSUALES</h2>
+
+      <div className="flex-row gap-2 justify-center hidden md:flex">
+        <PlanCard
+          tituloBono="IRON"
+          precio="29€"
+          caracteristicas={["5 sesiones al mes"]}
+        />
+        <PlanCard
+          tituloBono="BRONZE"
+          precio="46€"
+          caracteristicas={["9 sesiones al mes"]}
+        />
+        <PlanCard
+          tituloBono="SILVER"
+          precio="57€"
+          caracteristicas={["13 sesiones al mes"]}
+        />
+        <PlanCard
+          tituloBono="GOLDEN"
+          precio="69€"
+          caracteristicas={["17 sesiones al mes"]}
+        />
+      </div>
+
+      <div className='md:hidden'>
+        <CarouselTarifas
+          components={[
+            <PlanCard key="iron" tituloBono="IRON" precio="29€" caracteristicas={["5 sesiones al mes"]} />,
+            <PlanCard key="bronze" tituloBono="BRONZE" precio="46€" caracteristicas={["9 sesiones al mes"]} />,
+            <PlanCard key="silver" tituloBono="SILVER" precio="57€" caracteristicas={["13 sesiones al mes"]} />,
+            <PlanCard key="golden" tituloBono="GOLDEN" precio="69€" caracteristicas={["17 sesiones al mes"]} />,
+          ]}
+          theme='blue'
+        />
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-4 px-12 2xl:px-30">
+        <PlanCard
+          tituloBono="MORNING DIAMOND"
+          precio="60€"
+          caracteristicas={[
+            "7 sesiones WOD / OPEN semanales",
+            "1 clase de GYMNASTIC semanal",
+            "1 clase de MOBILITY semanal",
+            "1 clase de HALTEROFILIA semanal",
+          ]}
+          alignLeft
+        />
+        <PlanCard
+          tituloBono="DIAMOND"
+          precio="79€"
+          caracteristicas={[
+            "7 sesiones WOD / OPEN semanales",
+            "1 clase de GYMNASTIC semanal",
+            "1 clase de MOBILITY semanal",
+            "1 clase de HALTEROFILIA semanal",
+          ]}
+          alignLeft
+        />
+        <ExtraCard
+          tituloBono="DIAMOND"
+          precio="79€"
+          caracteristicas={[
+            "1 clase de MOBILITY semanal",
+            "1 clase de HALTEROFILIA semanal",
+            "7 sesiones WOD / OPEN semanales",
+            "1 clase de GYMNASTIC semanal",
+          ]}
+          descripcionExtra="Los planes Diamond y Morning Diamond tienen disponible:"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default ClasesSection;
